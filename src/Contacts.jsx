13cc5d2b0cd93ecbe933f8496cf62a4eec684bc2
UtsavK-0112 <99528866+UtsavK-0112@users.noreply.github.com>
2024-05-25 10:51:05 +1000

    Add contacts page

--- src/Contacts.jsx
+++ src/Contacts.jsx
@@ -1,8 +1,62 @@
 import EmbeddedPage from "./components/EmbeddedPage";
+import { Email } from "@mui/icons-material";
+import { Instagram } from "@mui/icons-material";
+import { Map } from "@mui/icons-material";
+import "./styles/Contacts.css";
 
 function Contacts() {
     return (
-        <EmbeddedPage embedLink="https://hahstechclub.github.io/tech-club-contacts/" />
+        <div className="contacts">
+            <h1>Contacts</h1>
+            <table>
+                <tr>
+                    <td>
+                        <Email></Email>
+                    </td>
+                    <td>Official Tech Club Email</td>
+                    <td>hahs.techclub@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>
+                        <Email></Email>
+                    </td>
+                    <td>Supervising Teachers' Email</td>
+                    <td>
+                        dijendra.nand4@det.nsw.edu.au <br />
+                        mark.zarzour3@det.nsw.edu.au
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <Email></Email>
+                    </td>
+                    <td>Current Executives' Email</td>
+                    <td>
+                        utsav.kashichhwa@education.nsw.gov.au <br />
+                        karthik.sunderraj@education.nsw.gov.au <br />
+                        mikayla.rochadi@education.nsw.gov.au
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <Instagram></Instagram>
+                    </td>
+                    <td>Instagram</td>
+                    <td>@hahs.tech</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <Map></Map>
+                    </td>
+                    <td>Location</td>
+                    <td>
+                        Hurlstone Agricultural High School <br />
+                        Roy Watts Rd, Glenfield NSW 2167
+                    </td>
+                </tr>
+            </table>
+        </div>
     );
 }
 
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./styles/Layout.css";
 
 function Layout() {
     return (
         <div className="layout">
             <Navbar />
-            <div style={{ marginTop: "120px", height: "calc(100vh - 120px)" }}>
+            <div className="content">
                 <Outlet />
                 <Footer />
             </div>
